42679586fcec92eb60fc06b5609c7222954a7c3a
Carson Graham <carson42g@gmail.com>
2020-03-18 17:11:03 -0400

    more pointers

--- gen/inital.asm
+++ gen/inital.asm
@@ -1,31 +1,49 @@
 extern printf
+extern malloc
 
 section .text
     global main
 
 main:
-    mov ebx7
-    mov ecx, 107
-    call divide
-    mov ebx, eax
+    ;mov rbx, 702
+    mov rdi, 8 ; 8 bytes, one register
+
+    mov rax, 0
+    call malloc
+
+    mov [rax], TWORD 0xFFFF222233334444 ; TODO left off here
+                                        ; you can't put 64 bits into memory
+                                        ; but you can put a 64-bit register into memory
+                                        ; we should make r15 a free-to-use register
+                                        ; so we don't need to push/pop it every time we want to put 64 bits into memory
+                                        ; mov r15, 0xFFFF222233334444
+                                        ; mov [rax], r15
+                                        ; special cases
+                                        ; or, maybe we don't need that and we can just stick it into the accumulator
+                                        ; the special case is for writing to memory not reading from memory
+                                        ; reading is easy just
+                                        ; mov rax, ptrValueExpresionShti
+                                        ; mov rax, [rax]
+    mov [rax + 2], WORD 0x1111
+    mov rbx, 10
+    mov rbx, [rax]
     call println
     ret
 
-divide: ; return value in register eax
-    ; argument 0 (a) in register ebx
-    ; argument 1 (b) in register ecx
-    mov eax, ebx
-    push rbx
-    mov ebx, eax
-    mov eax, ecx
-    xor eax, ebx
-    xor ebx, eax
-    xor eax, ebx
+deref: ; return value in register eax
+    ; argument 0 (a) in register rbx
+    ; argument 1 (one) in register ecx
     push rdx
-    xor rdx, rdx
-    div ebx
+    mov rax, rbx
+    mov rax, [rax]
+    mov rax, [rax]
+    mov rax, [rax]
+    mov eax, [rax]
+    push rax
+    mov eax, ecx
     pop rdx
-    pop rbx
+    add eax, edx
+    pop rdx
     ret
 
 
@@ -42,4 +60,4 @@ println: ; function println(int64 i)
 
 
 section .data
-    string: db "%i", 10, 0
+    string: db "0x%x", 10, 0
