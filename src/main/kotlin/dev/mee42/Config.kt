82cb934a647fea3f2516c5c94cce941b5226c008
Carson Graham <carson42g@gmail.com>
2020-04-08 11:40:33 -0400

    started cli client

--- src/main/kotlin/dev/mee42/Config.kt
+++ src/main/kotlin/dev/mee42/Config.kt
@@ -3,9 +3,10 @@ package dev.mee42
 
 
 object Config {
-    enum class OptimizeLevel(val iterations: Int){
+    enum class OptimizeLevel(val iterations: Int) {
         O0(0), O1(1), O2(3), O3(10)
     }
+
     var optimizationLevel: OptimizeLevel = OptimizeLevel.O0
 
     private val flags = listOf<Flag>(
@@ -18,6 +19,7 @@ object Config {
 //            Flag.PRINT_PURITY
 //            Flag.VERBOSE
     ).flatMap { it.flatten() }.distinct()
+
     enum class Flag(private val implies: List<Flag> = emptyList()) {
         PRINT_END_TIMING,
         PRINT_COMPILE_TIMINGS,
@@ -30,18 +32,13 @@ object Config {
         PRINT_ALL_TIMINGS(listOf(PRINT_END_TIMING, PRINT_COMPILE_TIMINGS, PRINT_OPTIMIZATIONS)),
         VERBOSE(listOf(PRINT_END_TIMING, PRINT_COMPILE_TIMINGS, PRINT_OPTIMIZATIONS, PRINT_ASM_ID, PRINT_AST, DECOMPILE_AST, PRINT_LEXED, PRINT_PURITY))
         ;
+
         fun flatten(): List<Flag> {
             return (listOf(this) + this.implies.flatMap { it.flatten() }).distinct()
         }
     }
+
     fun isPicked(flag: Flag): Boolean {
         return flags.contains(flag)
     }
-    class Test(var x: String? = "hi")
-    fun test() {
-        val test: Test? = Test("goodbye")
-        test!!.x = null // set the thing to null
-    }
 }
-
-
--- src/main/kotlin/dev/mee42/arg/Main.kt
+++ src/main/kotlin/dev/mee42/arg/Main.kt
@@ -1,5 +1,156 @@
 package dev.mee42.arg
 
+import java.util.*
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
-    args.forEach { println(it) }
+    val config = parse(args.toList())
+    globalConfig = config
+    VerboseOption.CONFIG.println(config)
+}
+
+lateinit var globalConfig: Config
+
+enum class OutputFormat {
+    NASM, OBJECT, CODE
+}
+
+enum class Optimization(val str: String) {
+    DEAD_CODE("dead-code-elem"),
+    INLINE_MACROS("inline-macros"),
+    RESHUFFLE("reshuffle"),
+    VALUE_PROPAGATOR("value-propagator"),
+    FLATTEN_TYPELESS_BLOCK("flatten-typeless-blocks")
+}
+
+enum class VerboseOption(val str: String) {
+    END_TIME("end-timings"),
+    COMPILE_TIME("end-compile-timings"),
+    OPTIMIZATIONS("optimizations"),
+    AST("ast"),
+    DECOMPILE_AST("decompile-ast"),
+    TOKENS("tokens"),
+    PURITY("purity"),
+    CONFIG("config")
+    ;
+    fun println(str: String, err: Boolean = false) {
+        if(globalConfig.verboseEnabled.contains(this)){
+            (if(err) System.err else System.out).println(str)
+        }
+    }
+    fun println(any: Any?, err: Boolean = false) = println(any.toString(), err)
+
+}
+
+
+data class Config(val format: OutputFormat,
+                  val nasmCommand: String,
+                  val gccCommand: String,
+                  val buildDir: String,
+                  val optimizationsEnabled: List<Optimization>,
+                  val verboseEnabled: List<VerboseOption>,
+                  val compilerIterations: Int,
+                  val target: String?)
+
+
+
+
+class ConsumableQueue<T>(creator: Collection<T>) {
+    private val queue = ArrayDeque(creator)
+    private fun isEmpty() = queue.isEmpty()
+    fun peek(): T? = if(isEmpty()) null else queue.first
+    fun isNotEmpty() = queue.isNotEmpty()
+    fun remove(): T = if(isEmpty()) error("reached end of file while parsing") else queue.remove()
+    fun removeWhile(condition: (T) -> Boolean): List<T> {
+        val list = mutableListOf<T>()
+        while(true){
+            if(queue.isEmpty()) error("reached end of file while parsing")
+            if(condition(queue.peek())){
+                list.add(queue.remove())
+            } else {
+                return list
+            }
+        }
+    }
+}
+
+private fun parse(args: List<String>): Config {
+    var format: OutputFormat = OutputFormat.CODE
+    var nasmCommand = "nasm -felf64 {i} -o {o}"
+    var gccCommand  = "gcc -no-pie {i} -o {o}"
+    var buildDir    = "build/"
+    val optimizationsEnabled = mutableListOf<Optimization>()
+    val verboseEnabled = mutableListOf<VerboseOption>()
+    var compilerIterations = 0
+    var target: String? = null
+
+    val queue = ConsumableQueue(args)
+
+    fun sortaVerbose() {
+        verboseEnabled += listOf(VerboseOption.COMPILE_TIME, VerboseOption.END_TIME)
+    }
+
+    while(queue.isNotEmpty()) {
+        val token = queue.remove()
+        if(token.isBlank()) continue
+        when {
+            token == "" -> {}
+            token == "--help" -> {
+                // execute man xenon, and return
+                println("[help menu, todo]")
+                exitProcess(0)
+            }
+            token == "--nasm" -> nasmCommand = queue.remove()
+            token == "--gcc" -> gccCommand = queue.remove()
+            token == "--build" -> buildDir = queue.remove()
+            token == "--format" -> {
+                val rem = queue.remove()
+                val opt = OutputFormat.values().firstOrNull { it.name.toUpperCase() == rem.toUpperCase() }
+                        ?: error("can't find output format \"$rem\"")
+                format = opt
+            }
+            token == "-t" || token == "--target" -> target = queue.remove()
+            token == "--verbose, -v" -> sortaVerbose()
+            token == "-vv" || token == "-vall" -> verboseEnabled += VerboseOption.values()
+            token.startsWith("-v") -> {
+                val (str, remove) = if(token.startsWith("-vno")) {
+                    token.substring(4) to true
+                } else token.substring(2) to false
+                val option = VerboseOption.values().firstOrNull { it.str.toUpperCase() == str.toUpperCase() }
+                        ?: error("Can't find verbose option" + token.substring(2).toUpperCase())
+                if(remove){
+                    verboseEnabled.remove(option)
+                } else {
+                    verboseEnabled.add(option)
+                }
+            }
+            token.startsWith("-O") -> {
+                val i = token.substring(2).toIntOrNull() ?: error("can't convert \"${token.substring(2)}\" to an int")
+                compilerIterations = i
+                if(i != 0){
+                    optimizationsEnabled += Optimization.values()
+                }
+            }
+            token.startsWith("-o") || token == "--optimize" -> {
+                val (str, remove) = when {
+                    token == "--optimize" -> {
+                        val rem =  queue.remove()
+                        if(rem.startsWith("no")) rem.substring(2) to true else rem to false
+                    }
+                    token.startsWith("-ono") -> token.substring(4) to true
+                    else -> token.substring(2) to false
+                }
+                val option = Optimization.values().firstOrNull { it.str.toUpperCase() == str.toUpperCase() }
+                        ?: error("Can't find verbose option" + token.substring(2).toUpperCase())
+                if(remove){
+                    optimizationsEnabled.remove(option)
+                } else {
+                    optimizationsEnabled.add(option)
+                }
+            }
+
+        }
+    }
+
+    return Config(format, nasmCommand, gccCommand, buildDir, optimizationsEnabled, verboseEnabled, compilerIterations, target)
 }
